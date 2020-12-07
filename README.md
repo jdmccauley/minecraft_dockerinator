f7cbe22e1c5dcff5589f3f9a496d49535324142f
Joshua McCauley <jdm.mccauley@gmail.com>
2020-12-06 23:39:15 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Welcome to the
+               _                            __ _   
+     _ __ ___ (_)_ __   ___  ___ _ __ __ _ / _| |_
+    | '_ ` _ \| | '_ \ / _ \/ __| '__/ _` | |_| __|
+    | | | | | | | | | |  __/ (__| | | (_| |  _| |_
+    |_| |_| |_|_|_| |_|\___|\___|_|  \__,_|_|  \__|
+
+         _            _             _             _
+      __| | ___   ___| | _____ _ __(_)_ __   __ _| |_ ___  _ __
+     / _` |/ _ \ / __| |/ / _ \ '__| | '_ \ / _` | __/ _ \| '__|
+    | (_| | (_) | (__|   <  __/ |  | | | | | (_| | || (_) | |
+     \__,_|\___/ \___|_|\_\___|_|  |_|_| |_|\__,_|\__\___/|_|
+
+    
+# Quickstart
+
+```
+python3 dockerizer.py
+```
+
+# Purpose
+
+The purpose of this repo is to automate the migration of an existing world in Minecraft Bedrock Edition to a Docker volume. This way, the volume can be mounted on a container that serves [Minecraft Bedrock Edition.](https://hub.docker.com/r/itzg/minecraft-bedrock-server)
+
+This python script finds your Minecraft worlds on your PC, lets you pick which one to convert to a volume, and copies it to a Docker volume. This can then be run with the server container to host your Minecraft world.
+
+# Supported OS'es
+
+* Windows 10
+
+# Dependencies
+
+* Minecraft Bedrock Edition 
+
+* [Minecraft Bedrock Server Docker container by itzg](https://hub.docker.com/r/itzg/minecraft-bedrock-server)
+
+* Docker
+
+* Python3
+
+# Assumptions
+
+* World is saved on a C drive at it's default location, as per [source](https://gaming.stackexchange.com/questions/330407/where-does-minecraft-for-windows-10-store-its-data):
+```
+C:\Users\username\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftWorlds
+```
+
+
+# Steps:
+
+1. Clone this repo.
+2. Run the python script from the repo with `python3 dockerizer.py`.
+    * The Bedrock server is started automatically.
+3. Open Minecraft, go to 'Play -> Friends -> LAN Games -> Dedicated Server'
+4. Play Minecraft with your friends (or alone, that's fun too)!
---
+++ dockerinator.py
@@ -0,0 +1,110 @@
+# The purpose of this script is to copy a user's Minecraft Bedrock Edition's world data into a docker volume.
+# This docker volume will be configured to run the world in a container that serves the world.
+
+# import libraries
+import os
+import shutil
+import subprocess
+import io
+import getpass
+
+# global variables
+utf8 = 'utf-8'
+
+# functions
+
+def get_world_dirs() -> list:
+    username = getpass.getuser()
+    user_path = f'C:\\Users\\{username}\\AppData\\Local\\Packages\\Microsoft.MinecraftUWP_8wekyb3d8bbwe\\LocalState\\games\\com.mojang\\minecraftWorlds'
+    world_dirs = []
+    for dir in os.listdir(user_path):
+        if os.path.isdir(os.path.join(user_path, dir)) == True:
+            world_dirs.append(dir)
+    for i in range(len(world_dirs)):
+        world_dirs[i] = f'{user_path}\{world_dirs[i]}'
+    return world_dirs
+
+def get_world_names(world_dirs: list) -> list:
+    world_names = []
+    for i in range(len(world_dirs)):
+        f = open(f'{world_dirs[i]}/levelname.txt')
+        world_names.append(
+            f.readlines()[0]
+        )
+        f.close()
+    return world_names
+
+
+def pick_world(world_names: list) -> str:
+    for i in range(len(world_names)):
+        print(f'{i + 1}: {world_names[i]}')
+    selection = input("Enter which Minecraft world you want to serve: ")
+    selection = int(selection) - 1
+    return selection
+
+def make_volume(
+    world_dirs: list, 
+    picked_world: str
+) -> None:
+    picked_path = world_dirs[picked_world]
+    # make temporary directory with world information
+    tmpdir = os.path.join(os.getcwd(), 'data')
+    if 'data' in os.listdir():
+        shutil.rmtree(tmpdir)
+    shutil.copytree(picked_path, f'{tmpdir}/worlds/Bedrock level')
+    shutil.copyfile('server.properties', 'data/server.properties')
+    # delete if volume already exists
+    volume_name = 'dockerized_world'
+    volumes = subprocess.check_output([
+        'powershell.exe',
+        'docker volume ls'],
+        text = True
+            ).replace(" ", "\t"
+            ).replace("\n", ""
+            ).split("\t"
+    )
+    if volume_name in volumes:
+        subprocess.run([
+            'powershell.exe',
+            f'docker volume rm {volume_name}'
+        ])
+    # make new volume
+    subprocess.run([
+        'powershell.exe',
+        f'docker volume create {volume_name}'
+    ])
+    # add volumes to volume
+    subprocess.run([
+        'powershell.exe',
+        'docker create -it -v dockerized_world:/data --name file_shipper ubuntu',
+    ])
+    subprocess.run([
+        'powershell.exe',
+        'docker cp ./data file_shipper:/',
+    ])
+    subprocess.run([
+        'powershell.exe',
+        'docker cp ./data/server.properties file_shipper:/data/server.properties'
+    ])
+    subprocess.run([
+        'powershell.exe',
+        'docker rm file_shipper'
+    ])
+    shutil.rmtree(tmpdir)
+
+# main
+world_dirs = get_world_dirs()
+world_names = get_world_names(world_dirs)
+
+world = pick_world(world_names)
+print(f'You picked {world_names[world]}.')
+
+make_volume(world_dirs, world)
+
+subprocess.run([
+    'powershell.exe',
+    'docker run -it -d -e EULA=TRUE -p 19132:19132/udp -v dockerized_world:/data --name dockerized_minecraft itzg/minecraft-bedrock-server'
+])
+
+
+
